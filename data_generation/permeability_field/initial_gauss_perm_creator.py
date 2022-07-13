cb06459e5788f36005cfecaf353e24aca5027eb4
leiterrl <leiterrl@ipvs.uni-stuttgart.de>
2022-07-13 20:46:07 +0200

    format everything

--- data_generation/permeability_field/initial_gauss_perm_creator.py
+++ data_generation/permeability_field/initial_gauss_perm_creator.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import Rbf
 from FyeldGenerator import generate_field
 from matplotlib import use
-import random 
+import random
 
 
-use('Agg')
+use("Agg")
 
 
 # Helper that generates power-law power spectrum
@@ -25,18 +25,21 @@ def Pkgen(n):
 
     return Pk
 
+
 # Draw samples from a normal distribution
 def distrib(shape):
     a = np.random.normal(loc=0, scale=1, size=shape)
     b = np.random.normal(loc=0, scale=1, size=shape)
     c = np.random.normal(loc=0, scale=1, size=shape)
-    return a*c + 1j * b
+    return a * c + 1j * b
+
 
 def rescaleArray(array):
     a_min = np.min(array)
     a_max = np.max(array)
-    norm_array = (1*(array - a_min) / (a_max - a_min) ) - 0
+    norm_array = (1 * (array - a_min) / (a_max - a_min)) - 0
     return norm_array
+
 
 ###########################################
 # variables for setup #####################
@@ -48,13 +51,13 @@ cell_centers_path = "grid_cell_centers.dat"
 # mean initial permeability value
 const_permeability = 1e-9
 
-#scaling factor to increase permeability field differences
+# scaling factor to increase permeability field differences
 scale_factor = 2
-plot=False
+plot = False
 
 # permeability dataset file name
 # has to match the link in the PFLOTRAN input file
-filename = 'permeability.h5'
+filename = "permeability.h5"
 
 
 x = []
@@ -66,50 +69,50 @@ xOut = []
 yOut = []
 
 width = 4
-WidthSmall = width*1.2
-n = 6		# Number of points on coarse grid
-nOut = 64	# Number of points in PFLOTRAN grid
+WidthSmall = width * 1.2
+n = 6  # Number of points on coarse grid
+nOut = 64  # Number of points in PFLOTRAN grid
 
 nOutput = []
 
 plt.figure()
 
-for i in range(0,n):
-    for j in range(0,n):
+for i in range(0, n):
+    for j in range(0, n):
         nOutput = random.uniform(-1, 1)
-        if (i == 0):
-            x.append((-1)*(WidthSmall/n))
-        elif (i == n-1):
-            x.append((i+1)*(WidthSmall/n))
+        if i == 0:
+            x.append((-1) * (WidthSmall / n))
+        elif i == n - 1:
+            x.append((i + 1) * (WidthSmall / n))
         else:
-            x.append(i*(WidthSmall/n))
-        if (j == 0):
-            y.append(-1*(WidthSmall/n))
-        elif (j == n-1):
-            y.append((j+1)*(WidthSmall/n))
+            x.append(i * (WidthSmall / n))
+        if j == 0:
+            y.append(-1 * (WidthSmall / n))
+        elif j == n - 1:
+            y.append((j + 1) * (WidthSmall / n))
         else:
-            y.append(j*(WidthSmall/n))
-        #y.append(j)
-        #field.append(0)
-        
-for i in range(0,nOut):
-    for j in range(0,nOut):
-        xOut.append(i*(width/nOut))
-        yOut.append(j*(width/nOut))
-        
+            y.append(j * (WidthSmall / n))
+        # y.append(j)
+        # field.append(0)
+
+for i in range(0, nOut):
+    for j in range(0, nOut):
+        xOut.append(i * (width / nOut))
+        yOut.append(j * (width / nOut))
+
 xOut = np.asarray(xOut)
 yOut = np.asarray(yOut)
-#field = np.asarray(x)
+# field = np.asarray(x)
 
 # Create shapes for generating the permeabilityy field
-shape = (n,n)
+shape = (n, n)
 shapeOut = (nOut, nOut)
 
 field = generate_field(distrib, Pkgen(2), shape)
 print("Min field: ", field.min())
-#np.interp(field, (field.min(), field.max()), (-2, +2))
-field[:,0] = rescaleArray(field[:,0])
-field[:,1] = rescaleArray(field[:,1])
+# np.interp(field, (field.min(), field.max()), (-2, +2))
+field[:, 0] = rescaleArray(field[:, 0])
+field[:, 1] = rescaleArray(field[:, 1])
 print("Min field: ", field.min())
 
 
@@ -124,48 +127,48 @@ print("Min Y fine: ", min(yOut))
 print(x)
 print(y)
 print(field)
-#input()
-#for i in range(0,n):
+# input()
+# for i in range(0,n):
 #    x[i] *= 10
-#    y[i] *= 10     
+#    y[i] *= 10
 
 # New method: Generate random values at each coarse mesh point
 count = 0
-for i in range(0,n):
-    for j in range(0,n):
-        field[i,j] = random.uniform(-1, 1)
+for i in range(0, n):
+    for j in range(0, n):
+        field[i, j] = random.uniform(-1, 1)
         count += 1
-        
-# Create RBF interpolation field   
-rbf = Rbf(x, y, field, function='thin_plate')
+
+# Create RBF interpolation field
+rbf = Rbf(x, y, field, function="thin_plate")
 # Evaluate RBF on PFLOTRAN mesh
-output_values = rbf(xOut,yOut)
+output_values = rbf(xOut, yOut)
 
 
 fieldOut = generate_field(distrib, Pkgen(4), shapeOut)
 # Replace PFLOTRAN field output with RBF interpolation values
 count = 0
-for i in range(0,nOut):
-    for j in range(0,nOut):
-        fieldOut[i,j] = output_values[count]
+for i in range(0, nOut):
+    for j in range(0, nOut):
+        fieldOut[i, j] = output_values[count]
         count += 1
-        
-#print(max(fieldOut[0]))
-#print(max(fieldOut[1]))
-#print(min(fieldOut[0]))
-#print(min(fieldOut[1]))
 
-plt.imshow(field, cmap='jet')
+# print(max(fieldOut[0]))
+# print(max(fieldOut[1]))
+# print(min(fieldOut[0]))
+# print(min(fieldOut[1]))
 
-plt.savefig('field.png',dpi=400)
+plt.imshow(field, cmap="jet")
+
+plt.savefig("field.png", dpi=400)
 plt.close()
 
 plt.figure()
-plt.imshow(fieldOut, cmap='jet')
-plt.savefig('fieldOut.png',dpi=400)
+plt.imshow(fieldOut, cmap="jet")
+plt.savefig("fieldOut.png", dpi=400)
 plt.close()
 
-#exit()
+# exit()
 
 # END #####################################
 
@@ -176,18 +179,19 @@ def normalize(array):
     norm_array = (array - a_min) / (a_max - a_min)
     return norm_array
 
+
 def initial_permeability_variation(x, y):
     print(x)
     """calculate example permeability field for calibration testing"""
-    #output_values = rbf(x*(n-1),y*(n-1))
-    #var_val = 4 * x * (1 - x) * np.cos(3 * np.pi * x) * np.sin(3 * np.pi * y)
-    #output_values = rescaleArray(output_values)
-    #output_values[:,1] = rescaleArray(output_values[:,1])
+    # output_values = rbf(x*(n-1),y*(n-1))
+    # var_val = 4 * x * (1 - x) * np.cos(3 * np.pi * x) * np.sin(3 * np.pi * y)
+    # output_values = rescaleArray(output_values)
+    # output_values[:,1] = rescaleArray(output_values[:,1])
     var_val = rescaleArray(output_values)
     return -20 + (3 * var_val)
-    #return -19
-    
-    
+    # return -19
+
+
 #######################################
 # initial permeability dataset creation
 #######################################
@@ -195,34 +199,36 @@ def initial_permeability_variation(x, y):
 # get cell center coordinates for permeability interpolation
 if os.path.isfile(cell_centers_path):
     cell_center_coords = np.loadtxt(cell_centers_path)
-    perm_grid = initial_permeability_variation(normalize(cell_center_coords[:, 0]), normalize(cell_center_coords[:, 1]))
+    perm_grid = initial_permeability_variation(
+        normalize(cell_center_coords[:, 0]), normalize(cell_center_coords[:, 1])
+    )
     # Transform field values into the permeability field range
     perm_grid = np.exp(perm_grid)
-    #perm_grid_log = np.log(perm_grid)
-    #perm_grid = np.exp(perm_grid_log.mean() + ((perm_grid_log - perm_grid_log.mean()) * scale_factor))
+    # perm_grid_log = np.log(perm_grid)
+    # perm_grid = np.exp(perm_grid_log.mean() + ((perm_grid_log - perm_grid_log.mean()) * scale_factor))
     print("perm_grid: ", perm_grid)
     if plot:
         plt.scatter(cell_center_coords[:, 0], cell_center_coords[:, 1], c=perm_grid)
         plt.show()
-    print('allocating cell index array....')
+    print("allocating cell index array....")
     iarray = np.arange(1, cell_center_coords.shape[0] + 1, 1)
-    
-    print('setting cell indices....', len(iarray))
+
+    print("setting cell indices....", len(iarray))
     # add cell ids to file
-    dataset_name = 'Cell Ids'
-    h5file = File(filename, mode='w')
+    dataset_name = "Cell Ids"
+    h5file = File(filename, mode="w")
     h5dset = h5file.create_dataset(dataset_name, data=iarray)
     # add permeability to file
-    dataset_name = 'Permeability'
+    dataset_name = "Permeability"
     h5dset = h5file.create_dataset(dataset_name, data=perm_grid)
     h5file.close()
-    
+
     # debug output
-    print('min: %e' % np.max(perm_grid))
-    print('max: %e' % np.min(perm_grid))
-    print('ave: %e' % np.mean(perm_grid))
-    print('\n')
-    print('done with dummy permeability field ')
-    print('(used in initial simulation run to create dummy real world observations)')
+    print("min: %e" % np.max(perm_grid))
+    print("max: %e" % np.min(perm_grid))
+    print("ave: %e" % np.mean(perm_grid))
+    print("\n")
+    print("done with dummy permeability field ")
+    print("(used in initial simulation run to create dummy real world observations)")
 else:
     print("Error: No cell centers file found.")
--- data_generation/permeability_field/inputGenerator.py
+++ data_generation/permeability_field/inputGenerator.py
@@ -1,7 +1,8 @@
 from xlrd import open_workbook
 import os
 import matplotlib.pyplot as plt
-import random 
+import random
+
 
 class Region(object):
     def __init__(self, id, Point, X1, Y1, Z1, X2, Y2, Z2, nTemps):
@@ -16,20 +17,31 @@ class Region(object):
         self.nTemps = nTemps
 
     def __str__(self):
-        return("Region object:\n"
-               "  Well_Number = {0}\n"
-               "  Point = {1}\n"
-               "  X1 = {2}\n"
-               "  Y1 = {3}\n"
-               "  Z1 = {4}\n"
-               "  X2 = {5}\n"
-               "  Y2 = {6}\n"
-               "  Z2 = {7}\n"
-               "  n_Temps = {8}"
-               .format(self.id, self.Point, self.X1,
-                       self.Y1, self.Z1, self.X2, self.Y2, self.Z2, self.nTemps))
-                       
-SetwordsFirst = '''
+        return (
+            "Region object:\n"
+            "  Well_Number = {0}\n"
+            "  Point = {1}\n"
+            "  X1 = {2}\n"
+            "  Y1 = {3}\n"
+            "  Z1 = {4}\n"
+            "  X2 = {5}\n"
+            "  Y2 = {6}\n"
+            "  Z2 = {7}\n"
+            "  n_Temps = {8}".format(
+                self.id,
+                self.Point,
+                self.X1,
+                self.Y1,
+                self.Z1,
+                self.X2,
+                self.Y2,
+                self.Z2,
+                self.nTemps,
+            )
+        )
+
+
+SetwordsFirst = """
 #Description: 3D toy problem for richards equation
 
 SIMULATION
@@ -198,9 +210,9 @@ FLOW_CONDITION initial
   /
   DATUM 0.d0 0.d0 1.d0
   GRADIENT
-'''
+"""
 
-SetwordsSecond = '''  
+SetwordsSecond = """  
   /
   PRESSURE 101325.d0
   TEMPERATURE 10.d0
@@ -217,14 +229,14 @@ FLOW_CONDITION injection_1
     DATA_UNITS kg/s
     INTERPOLATION LINEAR
     #time  #massrate
-'''
-#    0.d0 0.01d0 
+"""
+#    0.d0 0.01d0
 #    40.d0 0.01d0
 #    80.d0 0.01d0
 #    120.d0 0.01d0
 #    320.d0 0.01d0
 
-SetwordsThird = '''
+SetwordsThird = """
 /
 #ENERGY_RATE 200 W
   TEMPERATURE LIST 
@@ -292,9 +304,9 @@ DEBUG
   PRINT_WAYPOINTS
   PRINT_COUPLERS
 END
-'''
+"""
 
-'''
+"""
 wb = open_workbook('testing.xls')
 i = 0
 for sheet in wb.sheets():
@@ -316,7 +328,7 @@ for sheet in wb.sheets():
                 values.append(value)
         item = Region(*values)
         items.append(item)
-'''
+"""
 
 p1 = []
 p2 = []
@@ -328,97 +340,109 @@ nRunsStart = 1
 nRunsEnd = 4
 
 # Generate random points for pressure field boundary condition
-while(len(p1) < (nRunsEnd - nRunsStart)):
-	print("Len p1: ", len(p1))
-	if (not skip == 0):
-		#rand1 = random.uniform(-3, -1)
-		#rand2 = random.uniform(-2, 2)
-		rand1 = random.uniform(-3, 3)
-		rand2 = random.uniform(-3, 3)
-		if (abs(rand1) > 1 and abs(rand2) > 1 ):
-			p1.append(rand1/8000)
-			p2.append(rand2/8000)
-			p3.append(0)
-    	#print("Accessing one single value (eg. DSPName): {0}".format(item.Point))
-		i = i + 1
-		skip += 1
-	skip += 1
+while len(p1) < (nRunsEnd - nRunsStart):
+    print("Len p1: ", len(p1))
+    if not skip == 0:
+        # rand1 = random.uniform(-3, -1)
+        # rand2 = random.uniform(-2, 2)
+        rand1 = random.uniform(-3, 3)
+        rand2 = random.uniform(-3, 3)
+        if abs(rand1) > 1 and abs(rand2) > 1:
+            p1.append(rand1 / 8000)
+            p2.append(rand2 / 8000)
+            p3.append(0)
+            # print("Accessing one single value (eg. DSPName): {0}".format(item.Point))
+        i = i + 1
+        skip += 1
+    skip += 1
 
-print ("P1: ", p1)
-print ("P2: ", p2)
+print("P1: ", p1)
+print("P2: ", p2)
 
-TS = 20		# Time step length for pflotran input temperatures
+TS = 20  # Time step length for pflotran input temperatures
 
 massFlow = 0
 bcCounter = -1
-for i in range(nRunsStart,nRunsEnd):
-	bcCounter += 1
-	os.system("rm -r permeability.h5")
-	os.system("python3 initial_gauss_perm_creator.py")
-	if (i >= nRunsStart):
-		# Need to run 1 simulation without GWHP flow and 1 with flow
-		for j in range(0,2):
-			print("Running Simulation J: ", j, " - With Inputs: ", i)
-			print("bcCounter: ", bcCounter)
-			if (j == 0):
-				massFlow = 0
-			else:
-				massFlow = 0.05
-				
-			file = open("pflotran.in","w")
-			file.write(SetwordsFirst)
-			file.write("    PRESSURE ")
-			file.write(str(p1[bcCounter]))
-			file.write(" ")
-			file.write(str(p2[bcCounter]))
-			file.write(" ")
-			file.write(str(p3[bcCounter]))
-			file.write(SetwordsSecond)
-			file.write("    0.d0 " + str(massFlow) + "d0" + "\n")
-			file.write("    40.d0 " + str(massFlow) + "d0" + "\n")
-			file.write("    80.d0 " + str(massFlow) + "d0" + "\n")
-			file.write("    120.d0 " + str(massFlow) + "d0" + "\n")
-			file.write("    320.d0 " + str(massFlow) + "d0")
-			file.write(SetwordsThird)
-			file.close()
-			print("Pressure Input: ", p1[i-nRunsStart], " , ", p2[i-nRunsStart])
-			os.system("mpirun -n 4 pflotran pflotran.in > log.pflotran")
-			#input()
-			if (j == 0):
-				os.system("cp pflotran.in results/pflotran-noFlow-" + str(i) + ".in")
-				os.system("cp pflotran-004.vtk results/pflotran-noFlow-" + str(i) +".vtk")
-				os.system("cp pflotran-vel-004.vtk results/pflotran-noFlow-vel-" + str(i) + ".vtk")
-				with open('cell.dat', 'r') as f1, open('results/pflotran-noFlow-'+str(i) +'.vtk', 'r') as f2, open('results/pflotran-noFlow-vel-'+str(i) + '.vtk', 'r') as f3, open('results/pflotran-noFlow-new-'+ str(i) + '.vtk', 'w') as newVTK, open('results/pflotran-noFlow-new-vel-'+ str(i) + '.vtk', 'w') as newVEL:
-					#input2 = f.read()
-					lines1 = f1.readlines()
-					lines2 = f2.readlines()
-					lines3 = f3.readlines()
-					newVTK.writelines(lines1[:])
-					newVTK.writelines(lines2[5:])
-					newVEL.writelines(lines1[:])
-					newVEL.writelines(lines3[5:])
-					
-				os.system("rm results/pflotran-noFlow-" + str(i) +".vtk")
-				os.system("rm results/pflotran-noFlow-vel-" + str(i) +".vtk")
-				os.system("rm results/pflotran-withFlow-" + str(i) +".vtk")
-				os.system("rm results/pflotran-withFlow-vel-" + str(i) +".vtk")
-			else:
-				os.system("cp pflotran.in results/pflotran-withFlow-" + str(i) + ".in")
-				os.system("cp pflotran-004.vtk results/pflotran-withFlow-" + str(i) +".vtk")
-				os.system("cp pflotran-vel-004.vtk results/pflotran-withFlow-vel-" + str(i) + ".vtk")
-				with open('cell.dat', 'r') as f1, open('results/pflotran-withFlow-'+str(i) +'.vtk', 'r') as f2, open('results/pflotran-withFlow-vel-'+str(i) + '.vtk', 'r') as f3, open('results/pflotran-withFlow-new-'+ str(i) + '.vtk', 'w') as newVTK, open('results/pflotran-withFlow-new-vel-'+ str(i) + '.vtk', 'w') as newVEL:
-					#input2 = f.read()
-					lines1 = f1.readlines()
-					lines2 = f2.readlines()
-					lines3 = f3.readlines()
-					newVTK.writelines(lines1[:])
-					newVTK.writelines(lines2[5:])
-					newVEL.writelines(lines1[:])
-					newVEL.writelines(lines3[5:])
-					
-				os.system("rm results/pflotran-noFlow-" + str(i) +".vtk")
-				os.system("rm results/pflotran-noFlow-vel-" + str(i) +".vtk")	
-				os.system("rm results/pflotran-withFlow-" + str(i) +".vtk")
-				os.system("rm results/pflotran-withFlow-vel-" + str(i) +".vtk")
-				
+for i in range(nRunsStart, nRunsEnd):
+    bcCounter += 1
+    os.system("rm -r permeability.h5")
+    os.system("python3 initial_gauss_perm_creator.py")
+    if i >= nRunsStart:
+        # Need to run 1 simulation without GWHP flow and 1 with flow
+        for j in range(0, 2):
+            print("Running Simulation J: ", j, " - With Inputs: ", i)
+            print("bcCounter: ", bcCounter)
+            if j == 0:
+                massFlow = 0
+            else:
+                massFlow = 0.05
 
+            file = open("pflotran.in", "w")
+            file.write(SetwordsFirst)
+            file.write("    PRESSURE ")
+            file.write(str(p1[bcCounter]))
+            file.write(" ")
+            file.write(str(p2[bcCounter]))
+            file.write(" ")
+            file.write(str(p3[bcCounter]))
+            file.write(SetwordsSecond)
+            file.write("    0.d0 " + str(massFlow) + "d0" + "\n")
+            file.write("    40.d0 " + str(massFlow) + "d0" + "\n")
+            file.write("    80.d0 " + str(massFlow) + "d0" + "\n")
+            file.write("    120.d0 " + str(massFlow) + "d0" + "\n")
+            file.write("    320.d0 " + str(massFlow) + "d0")
+            file.write(SetwordsThird)
+            file.close()
+            print("Pressure Input: ", p1[i - nRunsStart], " , ", p2[i - nRunsStart])
+            os.system("mpirun -n 4 pflotran pflotran.in > log.pflotran")
+            # input()
+            if j == 0:
+                os.system("cp pflotran.in results/pflotran-noFlow-" + str(i) + ".in")
+                os.system("cp pflotran-004.vtk results/pflotran-noFlow-" + str(i) + ".vtk")
+                os.system("cp pflotran-vel-004.vtk results/pflotran-noFlow-vel-" + str(i) + ".vtk")
+                with open("cell.dat", "r") as f1, open(
+                    "results/pflotran-noFlow-" + str(i) + ".vtk", "r"
+                ) as f2, open("results/pflotran-noFlow-vel-" + str(i) + ".vtk", "r") as f3, open(
+                    "results/pflotran-noFlow-new-" + str(i) + ".vtk", "w"
+                ) as newVTK, open(
+                    "results/pflotran-noFlow-new-vel-" + str(i) + ".vtk", "w"
+                ) as newVEL:
+                    # input2 = f.read()
+                    lines1 = f1.readlines()
+                    lines2 = f2.readlines()
+                    lines3 = f3.readlines()
+                    newVTK.writelines(lines1[:])
+                    newVTK.writelines(lines2[5:])
+                    newVEL.writelines(lines1[:])
+                    newVEL.writelines(lines3[5:])
+
+                os.system("rm results/pflotran-noFlow-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-noFlow-vel-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-withFlow-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-withFlow-vel-" + str(i) + ".vtk")
+            else:
+                os.system("cp pflotran.in results/pflotran-withFlow-" + str(i) + ".in")
+                os.system("cp pflotran-004.vtk results/pflotran-withFlow-" + str(i) + ".vtk")
+                os.system(
+                    "cp pflotran-vel-004.vtk results/pflotran-withFlow-vel-" + str(i) + ".vtk"
+                )
+                with open("cell.dat", "r") as f1, open(
+                    "results/pflotran-withFlow-" + str(i) + ".vtk", "r"
+                ) as f2, open("results/pflotran-withFlow-vel-" + str(i) + ".vtk", "r") as f3, open(
+                    "results/pflotran-withFlow-new-" + str(i) + ".vtk", "w"
+                ) as newVTK, open(
+                    "results/pflotran-withFlow-new-vel-" + str(i) + ".vtk", "w"
+                ) as newVEL:
+                    # input2 = f.read()
+                    lines1 = f1.readlines()
+                    lines2 = f2.readlines()
+                    lines3 = f3.readlines()
+                    newVTK.writelines(lines1[:])
+                    newVTK.writelines(lines2[5:])
+                    newVEL.writelines(lines1[:])
+                    newVEL.writelines(lines3[5:])
+
+                os.system("rm results/pflotran-noFlow-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-noFlow-vel-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-withFlow-" + str(i) + ".vtk")
+                os.system("rm results/pflotran-withFlow-vel-" + str(i) + ".vtk")
